2e9dddd198ff4e34ca9c3395cdf1fa94c30ad5f3
Migueliot1 <migueliot@protonmail.com>
2022-03-17 03:20:21 +0300

    Change classes' comments to docstrings.

--- calc.py
+++ calc.py
@@ -1,30 +1,39 @@
 # This file contains all the classes which are needed for this app
 
-# Represents a bill which flatmates need to pay for
 class Bill:
+    '''Represents a bill object which flatmates need to pay for.
+    Contains such data as amount and period of payment'''
 
-    # Create an instance with 2 parameters: amount of money to pay and period of time to pay for
     def __init__(self, amount, period):
-
+        '''Create an instance with 2 parameters:
+        amount of money to pay and period of time (days) to pay for'''
         self.amount = amount
         self.period = period
 
-# Represents each flatmate who needs to pay for a bill
-class Flatmate:
 
-    # Create an instance with 2 parameters: name of flatmate and days they spent in the house
+class Flatmate:
+    '''Represents each individual flatmate
+    who needs to pay for a bill'''
+
     def __init__(self, name, days_in_house):
+        '''Create an instance with 2 parameters:
+        name of flatmate and days they spent in the house'''
 
         self.name = name
         self.days_in_house = days_in_house
 
-# Represents a pdf report file which would be generated with how much each flatmate needs to pay
+
 class PdfReport:
+    '''Represents a pdf report file which would be generated 
+    with how much each flatmate needs to pay from a bill'''
 
     def __init__(self, filename):
+        '''Create an instance with a filename as a parameter'''
 
         self.filename = filename
 
     def generate(self, bill, flatmate1, flatmate2):
+        '''Generate a pdf file from given data from bill
+        and flatmates'''
 
-        return
+        pass
